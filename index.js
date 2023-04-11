6ad1ce614c2396c9e0389eaba0296f2e65909cf0
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-04-11 19:25:03 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected.");
+
+  socket.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected.");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
